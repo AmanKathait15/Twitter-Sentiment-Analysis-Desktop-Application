41f716fbaedabd7242e8d00353ae4edb684ef3a1
Aman Kathait <agnikathait171@gmail.com>
2020-05-09 09:54:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+
+
+
+consumer_key = '6Z8LgFMzu2XU7bmdzPa0SEiLm'
+        
+consumer_secret = 'ObHhjtcQwcCUnJN2eL6XwPUVsQtW0agMVzBC5KibExaZatwI2n'
+
+access_token = '1167253468165206016-m6cLVMNwTQW3qJfKgmvia7OndlmK5Y'
+
+access_token_secret = '6CNz9VZoNGdShbJv7C99edorxKtu6LiS8ZZ5Ei4NdJzvU'
